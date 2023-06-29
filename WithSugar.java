1d77b9a05b445e9e7977e8308695d7809a482f4a
Sam Braude <sambraude@gmail.com>
2023-06-28 21:21:49 -0700

    Update WithSugar.java

---
+++ WithSugar.java
@@ -0,0 +1,32 @@
+/*
+ * Author: Sam Braude (Red ID: 826984009)
+ * Intermediate Computer Programming Lab (CS 160L)
+ * Erin Ratelle
+ * June 28, 2023
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class WithSugar extends CoffeeDecorator {
+    public WithSugar(Coffee c) {
+        super(c);
+    }
+
+    @Override
+    public double getCost() {
+        return super.getCost() + 0.15;
+    }
+
+    @Override
+    public List<String> getIngredients() {
+        List<String> ingredients = new ArrayList<>();
+        ingredients.add("Sugar");
+        return ingredients;
+    }
+
+    @Override
+    public String printCoffee() {
+        return super.printCoffee() + " with sugar";
+    }
+}
