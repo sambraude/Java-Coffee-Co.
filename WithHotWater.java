2def320825ac83286838afb2b74d1286cf3b4f79
Sam Braude <sambraude@gmail.com>
2023-06-28 21:21:20 -0700

    Update WithHotWater.java

---
+++ WithHotWater.java
@@ -0,0 +1,32 @@
+/*
+ * Author: Sam Braude (Red ID: 826984009)
+ * Intermediate Computer Programming Lab (CS 160L)
+ * Erin Ratelle
+ * June 28, 2023
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class WithHotWater extends CoffeeDecorator {
+    public WithHotWater(Coffee c) {
+        super(c);
+    }
+
+    @Override
+    public double getCost() {
+        return super.getCost();
+    }
+
+    @Override
+    public List<String> getIngredients() {
+        List<String> ingredients = new ArrayList<>();
+        ingredients.add("Hot Water");
+        return ingredients;
+    }
+
+    @Override
+    public String printCoffee() {
+        return super.printCoffee() + " with hot water";
+    }
+}
