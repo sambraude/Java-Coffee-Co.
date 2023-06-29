bf29f8c835c5b7ad57f27ab5303847498bc05462
Sam Braude <sambraude@gmail.com>
2023-06-28 21:20:54 -0700

    Update Statistics.java

---
+++ Statistics.java
@@ -0,0 +1,31 @@
+/*
+ * Author: Sam Braude (Red ID: 826984009)
+ * Intermediate Computer Programming Lab (CS 160L)
+ * Erin Ratelle
+ * June 28, 2023
+ */
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class Statistics {
+    private List<CoffeeOrder> orders;
+
+    public Statistics(List<CoffeeOrder> orders) {}
+
+    public double getTotal(LocalDateTime start, LocalDateTime end) {
+        return 0;
+    }
+
+    public double getAvgCost() {
+        return 0;
+    }
+
+    public String[] getPopularOrders(int maxRank) {
+        return null;
+    }
+
+    public String printStatistics() {
+        return "";
+    }
+}
